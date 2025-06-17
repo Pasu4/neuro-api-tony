533b47fa65b428a6556236fe2adf0f7a7b3e22bb
Pasu4 <pascalwieckhorst02@gmail.com>
2025-06-17 11:24:42 +0200

    1.6.1

--- src/neuro_api_tony/constants.py
+++ src/neuro_api_tony/constants.py
@@ -3,7 +3,7 @@
 from typing import Final
 
 APP_NAME: Final = "Tony"
-VERSION: Final = "1.6.0"
+VERSION: Final = "1.6.1"
 PACKAGE_NAME: Final = "neuro-api-tony"
 GIT_REPO_URL: Final = "https://github.com/Pasu4/neuro-api-tony"
 PYPI_URL: Final = "https://pypi.org/project/neuro-api-tony"
