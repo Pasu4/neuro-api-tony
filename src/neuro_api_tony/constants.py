516c8e02765a4ab199f2f76dce366a68cd2fcfe5
Pasu4 <pascalwieckhorst02@gmail.com>
2025-12-19 21:04:03 +0100

    2.1.0

--- src/neuro_api_tony/constants.py
+++ src/neuro_api_tony/constants.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import Final
 
 APP_NAME: Final = "Tony"
-VERSION: Final = "2.0.0"
+VERSION: Final = "2.1.0"
 PACKAGE_NAME: Final = "neuro-api-tony"
 GIT_REPO_URL: Final = "https://github.com/Pasu4/neuro-api-tony"
 GITHUB_RAW_URL: Final = "https://raw.githubusercontent.com/Pasu4/neuro-api-tony"
